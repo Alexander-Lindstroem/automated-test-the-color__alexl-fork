2de4c4eaf04ce13a5bf1bcbe9265a604b2c54a33
alelin <alexanderbdjgw@gmail.com>
2025-08-27 14:15:27 +0200

    added basic header component and page state

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+const Header = ({setCurrentPage}:HeaderProps) => {
+    return (
+        <header>
+            <div>
+                <h1>Color Picker</h1>
+            </div>
+            <div>
+                <h2>HOME</h2>
+                <h2>RGB</h2>
+                <h2>HEX</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header
