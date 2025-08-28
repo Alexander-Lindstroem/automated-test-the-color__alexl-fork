3f379fbe533eef060a6a94491e117549b7b1d645
alelin <alexanderbdjgw@gmail.com>
2025-08-28 13:52:37 +0200

    added clickable navigation and styling to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 const Header = ({setCurrentPage}:HeaderProps) => {
+    const handleClick = (argument:string) => {
+        setCurrentPage(argument)
+    }
+
     return (
-        <header>
+        <header className="flex border-b-black border-b-1 p-5 text-xl justify-around">
             <div>
                 <h1>Color Picker</h1>
             </div>
-            <div>
-                <h2>HOME</h2>
-                <h2>RGB</h2>
-                <h2>HEX</h2>
+            <div className="select-none flex flex-row gap-4">
+                <h2 className="cursor-pointer select-none" onClick={() => handleClick("home")}>HOME</h2>
+                <h2 className="cursor-pointer select-none" onClick={() => handleClick("rgb")}>RGB</h2>
+                <h2 className="cursor-pointer select-none" onClick={() => handleClick("hex")}>HEX</h2>
             </div>
         </header>
     )
